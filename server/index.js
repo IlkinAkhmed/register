b9d5e8c0c1ca0662b357601616c359d923a951b5
IlkinAkhmed <tu7mp4w99@code.edu.az>
2023-12-17 13:02:09 +0400

    login done

--- server/index.js
+++ server/index.js
@@ -8,19 +8,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(
-  session({
-    secret: "askle#@!!302_",
-    resave: false,
-    saveUninitialized: true,
-  })
-);
+
+app.use(session({
+  secret: "sdogj@#!##__45",
+  resave: false,
+  saveUninitialized: true
+}))
+
+
 
 const { Schema } = mongoose;
 
 const userSchema = new Schema(
   {
-    username: { type: String, required: true },
+    username: { type: String , minLength:[3,'username must be minimum 3 character'],maxLength:[15,'username must be maximum 15 character'] ,required: true },
     password: { type: String, required: true },
   },
   { timestamps: true }
@@ -37,25 +38,48 @@ app.get("/register", async (req, res) => {
   }
 });
 
-app.post("/register", async (req, res) => {
+// user  register
+app.post('/register', async (req, res) => {
   try {
-    const findeduser = await Users.findOne({ username: req.body.username });
-    if (findeduser) {
-      res.status(404).json({ message: "username already exist!! Try other username" });
-      return;
+    const findedUser = await Users.findOne({ username: req.body.username })
+    if (findedUser) {
+      res.send('Username already exist!! Try other Username')
+      return
     } else {
-      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+      const hashedPassword = await bcrypt.hash(req.body.password, 10)
       const user = new Users({
         username: req.body.username,
-        password: hashedPassword,
-      });
-      await user.save();
-      res.status(200).json({ message: "user created" });
+        password: hashedPassword
+      })
+      await user.save()
+      res.status(200).json({ message: "user created!" })
+
     }
   } catch (error) {
     res.status(500).json({ message: error });
+
   }
-});
+})
+
+// user login
+app.post('/login', async (req, res) => {
+  try {
+    const user = await Users.findOne({username:req.body.username})
+    if(user && bcrypt.compare(req.body.password,user.password)){
+      req.session.userId = user._id
+      res.status(200).send('User Sign In')
+    }
+
+  } catch (error) {
+    res.status(500).json({ message: error });
+
+  }
+})
+
+
+
+
+
 
 mongoose
   .connect(
